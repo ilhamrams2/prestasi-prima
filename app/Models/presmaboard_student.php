2e774bf49ac99a3c9f4cb434ec08fedee32dc43c
Your Name <albnnaardy11@gmail.com>
2025-10-13 14:18:21 +0700

    db isi

--- app/Models/presmaboard_student.php
+++ app/Models/presmaboard_student.php
@@ -8,4 +8,52 @@ use Illuminate\Database\Eloquent\Model;
 class presmaboard_student extends Model
 {
     use HasFactory;
+
+
+      protected $fillable = [
+        'nama',
+        'foto',
+        'kelas',
+        'jurusan',
+        'angkatan',
+        'email',
+        'no_induk',
+        'is_active',
+    ];
+
+
+    public function scores()
+    {
+        return $this->hasMany(presmaboard_score::class, 'student_id');
+    }
+
+    public function projects()
+    {
+        return $this->hasMany(presmaboard_project::class, 'student_id');
+    }
+
+    public function achievements()
+    {
+        return $this->hasMany(presmaboard_achievements::class, 'student_id');
+    }
+
+    public function leaderboards()
+    {
+        return $this->hasMany(presmaboard_leaderboard::class, 'student_id');
+    }
+
+    public function currentLeaderboard()
+    {
+        return $this->hasOne(presmaboard_leaderboard::class, 'student_id')
+                    ->latestOfMany(); // leaderboard terbaru
+    }
+
+    public function latestScore()
+    {
+        return $this->hasOne(presmaboard_score::class, 'student_id')
+                    ->latestOfMany();
+    }
+
+
+
 }
